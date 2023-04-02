44f3d88c39bb7c6533b067fdccce6df44091f812
Pholobo <ouyangphoenix@gmail.com>
2023-04-02 00:17:45 -0400

    adds homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,92 @@
 const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth')
 
 router.get('/', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render('homepage');
+  try {
+    const postData = await Post.findAll({
+      include: [
+        { 
+          model: User,
+          attributes: ['username']
+        }
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({
+      plain: true
+    }));
+
+    res.render('homepage', {
+      posts,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          include: [User]
+        }
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'Oh no! No post was found with that ID.' });
+      return;
+    }
+    const post = postData.get({
+      plain: true
+    });
+
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+
+    res.status(200);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const dashboard = userData.get({
+      plain: true
+    });
+
+    res.render('dashboard', {
+      ...dashboard,
+      logged_in: req.session.logged_in,
+    })
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
