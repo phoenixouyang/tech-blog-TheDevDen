f5fc15a92baa735a05f0fd0fecce301143082ce7
Pholobo <ouyangphoenix@gmail.com>
2023-04-03 23:43:57 -0400

    updates configs

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,15 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+  });
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
+    process.env.DATABASE,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: process.env.DB_HOST,
       dialect: 'mysql',
       port: 3306,
     }
