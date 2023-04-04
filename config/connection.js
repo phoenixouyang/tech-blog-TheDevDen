254b8baadba7f5282255b53042257d0e214e5168
Pholobo <ouyangphoenix@gmail.com>
2023-04-04 00:01:52 -0400

    updates to localhost

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,11 @@ if (process.env.JAWSDB_URL) {
   });
 } else {
   sequelize = new Sequelize(
-    process.env.DATABASE,
+    process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST,
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306,
     }
